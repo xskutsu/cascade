899ab6a28671f6e79ed7a3a3ee37fb45808d9e66
xskutsu <xsku@protonmail.com>
2025-08-16 19:19:56 -0500

    Entity

--- source/index.ts
+++ source/index.ts
@@ -35,3 +35,32 @@ export class CircleShape implements IShape {
 		this.inverseInertia = 1 / this.inertia;
 	}
 }
+
+export class Entity {
+	private static _identifierTicker: number = 0;
+
+	public readonly identifier: number = Entity._identifierTicker++;
+	public readonly shape: IShape;
+	public positionX: number;
+	public positionY: number;
+	public angle: number = 0;
+	public velocityX: number = 0;
+	public velocityY: number = 0;
+	public angularVelocity: number = 0;
+	public linearDamping: number = 0.05;
+	public angularDamping: number = 0.1;
+	constructor(shape: IShape, positionX: number, positionY: number) {
+		this.shape = shape;
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+
+	public update(deltaTime: number): void {
+		this.velocityX *= Math.exp(-this.linearDamping * deltaTime);
+		this.velocityY *= Math.exp(-this.linearDamping * deltaTime);
+		this.angularVelocity *= Math.exp(-this.angularDamping * deltaTime);
+		this.positionX += this.velocityX * deltaTime;
+		this.positionY += this.velocityY * deltaTime;
+		this.angle += this.angularVelocity * deltaTime;
+	}
+}
